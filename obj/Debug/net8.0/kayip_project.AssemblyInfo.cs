f5906521a14986b544ee72ed05c757b580cc83c6
boshra khalid <boshrakhalid6@gmail.com>
2024-06-20 21:34:07 +0300

    azura identity deletion

--- obj/Debug/net8.0/kayip_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/kayip_project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f5bc88d33c011f358c729b262660b661a62f3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a519bb681bc3cb3d8054aab0f95e5238b93534d")]
 [assembly: System.Reflection.AssemblyProductAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
