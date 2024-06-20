77f5bc88d33c011f358c729b262660b661a62f3a
boshra khalid <boshrakhalid6@gmail.com>
2024-06-20 21:24:27 +0300

    package deletion

--- obj/Debug/net8.0/kayip_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/kayip_project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395bab235fc1cd954cb56c2ad5db388a399b5d89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c5eb1276431afa42fea0902f52f2e66b34986c")]
 [assembly: System.Reflection.AssemblyProductAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
