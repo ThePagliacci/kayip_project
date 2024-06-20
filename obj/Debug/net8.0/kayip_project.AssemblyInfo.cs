60ae7042b504441d829356e7e9f4b6f3ddb5df7a
boshra khalid <boshrakhalid6@gmail.com>
2024-06-20 21:10:09 +0300

    package updates

--- obj/Debug/net8.0/kayip_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/kayip_project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079550572256674202509b3549f2a984446a598c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bebee2dc79d74983e191177f623ce93ec3f0cdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kayip_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
